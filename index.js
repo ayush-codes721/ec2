164bd16570687a9e68cc3c97953bfa151e419109
Ayush <ayushkumar@gradright.com>
2025-07-06 00:36:33 +0530

    uodates

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.post('/webhook', (req, res) => {
   res.status(200).json({ message: 'Webhook received' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
